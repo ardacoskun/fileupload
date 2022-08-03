2bf15fb56f7776834d12de990f36560278c694cb
Arda Coşkun <m.ardacoskun@gmail.com>
2022-08-04 00:04:17 +0300

    mongoose installed and connected with server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(() => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed.Server not started.");
+  });
