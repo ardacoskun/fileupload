a3ad789bc925fb9aa926b3cd2100c4e15e752184
Arda Coşkun <m.ardacoskun@gmail.com>
2022-08-05 23:08:56 +0300

    sizeConver funtion transfered to helpers folder

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,5 +1,6 @@
 const File = require("../models/File");
 const displayDate = require("../helpers/getDate");
+const sizeConvert = require("../helpers/sizeConvert");
 
 //Create New File
 const fileUpload = async (req, res) => {
@@ -26,20 +27,6 @@ const getFile = async (req, res) => {
   } catch (error) {
     res.status(400).send(error);
   }
-};
-
-const sizeConvert = (bytes, decimal) => {
-  if (bytes === "") {
-    return "0 Bytes";
-  }
-  const sizeTypes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "YB", "ZB"];
-
-  const index = Math.floor(Math.log(bytes) / Math.log(1000));
-  return (
-    parseFloat((bytes / Math.pow(1000, index)).toFixed(decimal)) +
-    " " +
-    sizeTypes[index]
-  );
 };
 
 module.exports = { fileUpload, getFile };
