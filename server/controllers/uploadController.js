e32b6aa3f7a1bd6d8ce3bcc8c54c5f56fc0aef1e
Arda Coşkun <m.ardacoskun@gmail.com>
2022-08-08 02:12:44 +0300

    server getfiletype api added

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -13,6 +13,33 @@ const getFile = async (req, res) => {
   }
 };
 
+//Get File for Type
+const getFileType = async (req, res) => {
+  try {
+    const files = await File.find();
+
+    if (files) {
+      const type = req.params.type;
+      let filteredArr = [];
+      if (`${type}s`.toLowerCase() === "texts") {
+        filteredFiles = files.filter(
+          (file) =>
+            file.fileType.includes(type) ||
+            file.fileType.includes("application")
+        );
+        filteredArr.push(...filteredFiles);
+      } else {
+        filteredFiles = files.filter((file) => file.fileType.includes(type));
+        filteredArr.push(...filteredFiles);
+      }
+      return res.status(200).send(filteredArr);
+    }
+    return res.status(400).send("Files not found");
+  } catch (error) {
+    return res.status(500).send("Something went wrong!!");
+  }
+};
+
 //Delete File
 const deleteFile = async (req, res) => {
   try {
@@ -26,4 +53,4 @@ const deleteFile = async (req, res) => {
   }
 };
 
-module.exports = { getFile, deleteFile };
+module.exports = { getFile, deleteFile, getFileType };
